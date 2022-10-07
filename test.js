83ec98ba363b5a67ecb69c1f66345b9bc421219a
marijndegen <marijndegen96@hotmail.com>
2022-10-07 17:10:36 +0200

    Initial technical idea

--- test.js
+++ test.js
@@ -1,1 +1,51 @@
-console.log('hallo');
+class Playboard {
+    // Snake snake
+
+    // Bit isGameRunning
+}
+
+class Snake {
+    constructor() {
+        // should create two snake tiles (head and tail), put them in the snake array.
+    }
+
+    // The actual snake
+    // SnakeTile[] snakeArray
+
+    // To manage and calculate score, keep a score object
+    // Score score
+
+    // Bit isSnakeAlive
+}
+
+// Inherits from Tile
+class SnakeTile {
+    // (1 north, 2 east, 3 south, 4 west)
+    // int direction
+
+
+}
+
+// Inherits from Tile
+class EmptyTile {
+
+}
+
+// Inherits from Tile
+class Food {
+
+}
+
+// Abstract
+class Tile {
+    // string background
+}
+
+class Startbutton {
+
+}
+
+// No exponential score, just add one for each FoodTile gained
+class Score {
+    // int score
+}
